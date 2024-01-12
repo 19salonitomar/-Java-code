eed344cb06c38b2443a218db643221daf07133f0
19salonitomar <126148734+19salonitomar@users.noreply.github.com>
2024-01-12 17:35:05 +0530

    Create PyramidStar.java

---
+++ PyramidStar.java
@@ -0,0 +1,14 @@
+public class PyramidStar {
+    public static void main(String[] args) {
+        int i,j,n=6;
+        for(i=0;i<n;i++){// for row
+            for(j=n-i;j>1;j--){ //for space
+                System.out.print(" ");
+            }
+            for(j=0;j<=i;j++){//for column
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }    
+}
