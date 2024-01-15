13e9b68a3ac91e69a8f5e196beb31f993b35a6b0
19salonitomar <126148734+19salonitomar@users.noreply.github.com>
2024-01-15 10:19:21 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,15 @@
+public class Table{
+    public static void main(String[] args){
+        System.out.println("Table oF 10:");
+         int num = 10 ,res =1;
+         for(int i=1; i<num+1; i++){
+            res = i*num;
+         System.out.println("i*num" + "="+ res);
+        }
+        System.out.println("Print Numbers 1 to 10:");
+        for(int i=1; i<num+1; i++){
+            System.out.println(i);
+        }
+    
+    }
+}
