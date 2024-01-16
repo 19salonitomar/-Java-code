01903fdfd8c4d7f1a57baae61b2ab3a2a494ccab
19salonitomar <126148734+19salonitomar@users.noreply.github.com>
2024-01-16 15:42:20 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,9 @@
+public class Fibonacci {
+    public static void main(String args[]) {
+        int num = 10 , res =0;
+        for(int i = 0; i <=num; i++) {
+            res = res + i ;
+            System.out.println("Fibonacci Series:" + res);
+        }
+    }    
+}
