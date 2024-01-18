bb8872ebb2dc4209bb22caa9c935d8a55c3a2782
19salonitomar <126148734+19salonitomar@users.noreply.github.com>
2024-01-18 11:13:19 +0530

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,10 @@
+public class temperature {
+    public static void main(String[] args){
+        float Fahrenheit, Celsius;
+        Celsius = 24;
+        Fahrenheit = ((Celsius*9/5)+32);
+        System.out.println("Temperature in Fahrenheit: " + Fahrenheit);
+
+    } 
+    
+}
