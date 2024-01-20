cbaf6ba72b43d094eebceea31eb75b91e530ca3a
19salonitomar <126148734+19salonitomar@users.noreply.github.com>
2024-01-20 19:08:58 +0530

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public static void main(String[] args){
+        //Math function
+        System.out.println(Math.max(6,9));
+        System.out.println(Math.min(6,9));
+        //Randon Function which returns values from 0.0 to 1.0 in form of long double
+        System.out.println(Math.random());
+        //Randon Function with integer
+        System.out.println((int)(Math.random()*100));
+    }
+    
+    
+}
