56c204c77d30e3befd47772d11af3c96d2f582b5
Saloni <126148734+19salonitomar@users.noreply.github.com>
2024-02-11 23:16:24 +0530

    Arithmetics Operations

---
+++ Oaa.java
@@ -0,0 +1,57 @@
+public class Oaa {
+    public static void main(String[] args){
+        //Arithmetic Operators
+        //ADDITION
+        int a = 18;
+        int b = 19;
+        int add = a + b;
+        System.out.println(add);
+
+        //SUBRATIONAL
+        int c = 18;
+        int d = 19;
+        int diff = c - d;
+        System.out.println(diff);
+
+        //MULTIPLICATION
+        int e = 18;
+        int f = 19;
+        int mul = e*f ;
+        System.out.println(mul);
+
+        //DIVISION
+        int g = 18;
+        int h = 19;
+        int div = g/h ;
+        System.out.println(div);
+
+        //DIVISION WITH DECIMAL VALUES
+        double i = 18;
+        double j = 19;
+        double divi = i/j ;
+        System.out.println(divi);
+
+        //MODULO VALUES
+        double k = 25;
+        double l = 10;
+        double modulo = k % l;
+        System.out.println(modulo);
+
+        //Unary functions
+        int number = 2;
+        number++;
+        System.out.println(number);
+
+       // ++number  Pre- Increment
+       // number ++ Post- Increment
+
+        int Num = 2;
+        System.out.println (--Num);
+        System.out.println(Num); 
+            
+      
+        //  
+
+    }
+    
+}
